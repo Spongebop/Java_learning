23ddfec9dc8355f0d11256086987a4fb03afae3c
Spongebop <42912468+Spongebop@users.noreply.github.com>
2018-11-17 14:27:14 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Firstmodel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Telphone phone=new Telphone();
+        phone.sendMessage();
+	// write your code here
+    }
+}
---
+++ Telphone.java
@@ -0,0 +1,11 @@
+package Firstmodel;
+
+public class Telphone {
+    float CPU=1.5f;
+    float mome=2.0f;
+    int message=2;
+    void sendMessage()
+    {     int message=3;
+        System.out.println("我能输出短信"+this.message+"的值。");
+    }
+}
